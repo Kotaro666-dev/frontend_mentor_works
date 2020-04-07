4ba201f90b1039bf6471f4e0a23a2423e8838b92
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-04-07 20:43:19 +0900

    Fix price tag on javascript

--- pricing-component-with-toggle-master/index.js
+++ pricing-component-with-toggle-master/index.js
@@ -3,9 +3,9 @@ const priceBasic = document.getElementById("priceBasic");
 const pricePro = document.getElementById("pricePro");
 const priceMaster = document.getElementById("priceMaster");
 
-priceBasic.textContent = "19.99";
-pricePro.textContent = "24.99";
-priceMaster.textContent = "39.99";
+priceBasic.textContent = "199.99";
+pricePro.textContent = "249.99";
+priceMaster.textContent = "399.99";
 
 switchElement.addEventListener("click", ChangePrice);
 
@@ -13,12 +13,12 @@ let counter = 0;
 function ChangePrice() {
   counter++;
   if (counter % 2 == 0) {
-    priceBasic.textContent = "19.99";
-    pricePro.textContent = "24.99";
-    priceMaster.textContent = "39.99";
-  } else {
     priceBasic.textContent = "199.99";
     pricePro.textContent = "249.99";
     priceMaster.textContent = "399.99";
+  } else {
+    priceBasic.textContent = "19.99";
+    pricePro.textContent = "24.99";
+    priceMaster.textContent = "39.99";
   }
 }
