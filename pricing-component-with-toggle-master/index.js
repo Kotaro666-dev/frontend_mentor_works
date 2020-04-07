dee79db54ed3e09cbe37c7698bb5136269bedbb5
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-04-07 14:03:24 +0900

    Finish task

---
+++ pricing-component-with-toggle-master/index.js
@@ -0,0 +1,24 @@
+const switchElement = document.getElementById("toggleSwitch");
+const priceBasic = document.getElementById("priceBasic");
+const pricePro = document.getElementById("pricePro");
+const priceMaster = document.getElementById("priceMaster");
+
+priceBasic.textContent = "19.99";
+pricePro.textContent = "24.99";
+priceMaster.textContent = "39.99";
+
+switchElement.addEventListener("click", ChangePrice);
+
+let counter = 0;
+function ChangePrice() {
+  counter++;
+  if (counter % 2 == 0) {
+    priceBasic.textContent = "19.99";
+    pricePro.textContent = "24.99";
+    priceMaster.textContent = "39.99";
+  } else {
+    priceBasic.textContent = "199.99";
+    pricePro.textContent = "249.99";
+    priceMaster.textContent = "399.99";
+  }
+}
