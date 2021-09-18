2926beca416dea4501958f89bc37677c9e22cc0e
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-18 16:54:04 +0900

    Update: Add UserInput section

--- tip-calculator-app-main/src/App.js
+++ tip-calculator-app-main/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Wrapper from './Components/Helper/Wrapper';
 import UserInput from './Components/UserInput/UserInput'
 import Result from './Components/Result/Result'
@@ -6,12 +6,18 @@ import Title from './Components/Title/Title';
 import Card from './Components/UI/Card';
 
 function App() {
+	const [userInput, setUserInput] = useState({bill: '', tip: '', people: ''});
+
+	const setUserInputHandler = (bill, tip, people) => {
+		setUserInput({bill: bill, tip: tip, people: people});
+	}
+
 	return (
 		<Wrapper>
 			<Title />
 			<Card>
-				<UserInput />
-				<Result />
+				<UserInput setUserInput={setUserInputHandler}/>
+				<Result userInput={userInput}/>
 			</Card>
 		</Wrapper>
 	);
--- tip-calculator-app-main/src/Components/UserInput/UserInput.js
+++ tip-calculator-app-main/src/Components/UserInput/UserInput.js
@@ -1,13 +1,88 @@
-import React from "react";
-import Wrapper from "../Helper/Wrapper";
+import React, { useState } from "react";
 
 import classes from './UserInput.module.css';
+import Card from "../UI/Card";
+
+const tipsSelection = [
+	{id: '5%', percent: '5%', isSelected: false},
+	{id: '10%', percent: '10%', isSelected: false},
+	{id: '15%', percent: '15%', isSelected: false},
+	{id: '25%', percent: '25%', isSelected: false},
+	{id: '50%', percent: '50%', isSelected: false},
+	{id: 'Custom', percent: 'Custom', isSelected: false},
+];
 
 const UserInput = (props) => {
+	const [bill, setBill] = useState('');
+	const [people, setPeople] = useState('1');
+	const [tips, setTips] = useState(tipsSelection);
+	const [tip, setTip] = useState('');
+
+	const onChangeBill = (event) => {
+		setBill(event.target.value);
+	}
+
+	const onSelectTip = (id) => {
+		setTips((prevTips) => {
+			const newTips = prevTips.map((tip) =>
+				tip.id === id
+				? {id: tip.id, percent: tip.percent, isSelected: true}
+				: {id: tip.id, percent: tip.percent, isSelected: false}
+			);
+			return (newTips);
+		});
+		setTip(id);
+	}
+
+	const onChangePeople = (event) => {
+		setPeople(event.target.value);
+	}
+
+	const resetField = () => {
+		setBill('');
+		setTips(tipsSelection);
+		setTip('');
+		setPeople('1');
+	}
+
+	const addUserInput = (event) => {
+		event.preventDefault();
+
+		if (bill.trim().length === 0) {
+			console.log('bill empty');
+			return ;
+		}
+		if (tip.trim().length === 0) {
+			console.log('tip is not selected');
+			return ;
+		}
+		if (people.trim().length === 0) {
+			console.log('people empty');
+			return ;
+		}
+
+		props.setUserInput(bill, tip, people);
+		resetField();
+	}
+
 	return(
-		<Wrapper>
-			User input
-		</Wrapper>
+		<form onSubmit={addUserInput} className={classes.input}>
+			<label>Bill</label>
+			<input id="bill" type="text" onChange={onChangeBill} value={bill} className={classes.input}/>
+			<label>Select Tip %</label>
+			<Card className={classes.card}>
+				{
+					tips.map((tip) => (
+						<option key={tip.id} className={`${classes.tip_card} ${tip.isSelected && classes.tip_card_selected}`} onClick={() => onSelectTip(tip.id)}>
+							{tip.percent}
+						</option>
+					))
+				}
+			</Card>
+			<label>Number of People</label>
+			<input id="numberOfPeople" type="text" onChange={onChangePeople} value={people} className={classes.input} />
+			<button type="submit">Go</button>
+		</form>
 	);
 }
 
