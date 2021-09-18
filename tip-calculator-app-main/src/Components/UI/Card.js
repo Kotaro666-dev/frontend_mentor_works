ee4c7387b56467bca2652190158b72edb754b041
Kotaro Kamashima <kotaro0726@gmail.com>
2021-09-18 11:06:36 +0900

    Update: Add card components

--- tip-calculator-app-main/src/Components/UI/Card.js
+++ tip-calculator-app-main/src/Components/UI/Card.js
@@ -5,8 +5,8 @@ import classes from './Card.module.css';
 
 const Card = (props) => {
 	return(
-		<Wrapper>
-			props.children
+		<Wrapper className={`${classes.card} ${props.className}`}>
+			{props.children}
 		</Wrapper>
 	);
 }
